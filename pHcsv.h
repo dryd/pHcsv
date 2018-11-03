cf67371e5836f405f27b2d77e06f83c862dc4cd0
dryd <rydan4@gmail.com>
2018-11-03 21:50:31 +0100

    Write dynamic to file

--- pHcsv.h
+++ pHcsv.h
@@ -2,7 +2,8 @@
 #include <vector>
 #include <map>
 #include <iterator>
-#include <iostream>
+#include <fstream>
+#include <algorithm>
 
 namespace pHcsv {
 
@@ -18,11 +19,11 @@ inline std::string readCsvField(std::istreambuf_iterator<char>& it, bool& new_ro
   }
   if (*it == '"') {
     it++;
-    int consecutive_quotes = 0;
+    bool quote = false;
     while (it != EOCSVF) {
       char c = *it;
       it++;
-      if (consecutive_quotes) {
+      if (quote) {
         switch (c) {
           case '\n':
             new_row = true;
@@ -30,23 +31,19 @@ inline std::string readCsvField(std::istreambuf_iterator<char>& it, bool& new_ro
           case ',':
             return result;
           case '"':
-            consecutive_quotes++;
-            if (consecutive_quotes % 2 == 0) {
-              result.push_back('"');
-            }
+            result.push_back('"');
+            quote = false;
             break;
           default:
-            if (consecutive_quotes == 1) {
-              result.push_back('"');
-            }
+            result.push_back('"');
             result.push_back(c);
-            consecutive_quotes = 0;
+            quote = false;
             break;
           }
         } else {
           switch (c) {
             case '"':
-              consecutive_quotes = 1;
+              quote = true;
               break;
             default:
               result.push_back(c);
@@ -55,18 +52,40 @@ inline std::string readCsvField(std::istreambuf_iterator<char>& it, bool& new_ro
         }
     }
   } else {
+    bool quote = false;
     while (it != EOCSVF) {
       char c = *it;
       it++;
-      switch (c) {
-        case '\n':
-         new_row = true;
-          return result;
-        case ',':
-          return result;
-        default:
-          result.push_back(c);
-          break;
+      if (quote) {
+        switch (c) {
+          case '\n':
+            new_row = true;
+            return result;
+          case ',':
+            return result;
+          case '"':
+            quote = false;
+            break;
+          default:
+            result.push_back(c);
+            quote = false;
+            break;
+        }
+      } else {
+        switch (c) {
+          case '\n':
+            new_row = true;
+            return result;
+          case ',':
+            return result;
+          case '"':
+            result.push_back('"');
+            quote = true;
+            break;
+          default:
+            result.push_back(c);
+            break;
+        }
       }
     }
   }
@@ -82,55 +101,191 @@ inline std::vector<std::string> readCsvRow(std::istreambuf_iterator<char>& it) {
   return result;
 }
 
+void writeCsvRow(std::ostreambuf_iterator<char>& it, const std::vector<std::string>& row) {
+  for (size_t i = 0; i < row.size(); i++) {
+    const auto& field = row.at(i);
+    bool escape = false;
+    for (char c : field) {
+      if (c == ',' || c == '"') {
+        escape = true;
+      }
+    }
+    if (escape) {
+      it = '"';
+    }
+    for (char c : field) {
+      switch(c) {
+        case '"':
+          it = '"';
+          it = '"';
+          break;
+        default:
+          it = c;
+          break;
+      }
+    }
+    if (escape) {
+      it = '"';
+    }
+    if (i != row.size() - 1) {
+      it = ',';
+    }
+  }
+}
+
+template<typename T>
+inline T convert(const std::string& str) {
+  return T(str);  // hope it's constructible with a string...
+}
+
+template <>
+inline std::string convert(const std::string& str) {
+  return str;
+}
+
+template <>
+inline double convert(const std::string& str) {
+  return std::stod(str);
+}
+
+template <>
+inline float convert(const std::string& str) {
+  return std::stof(str);
+}
+
+template <>
+inline int convert(const std::string& str) {
+  return std::stoi(str);
+}
+
+template <>
+inline size_t convert(const std::string& str) {
+  return static_cast<size_t>(std::stoul(str));
+}
+
 }  // namespace detail
 
-class Dynamic {
+class dynamic {
  public:
-  Dynamic(std::istream& in, bool has_header) : indices_(), data_() {
+  dynamic(std::istream& in, bool has_header) : header_(), data_() {
+    readStream(in, has_header);
+  }
+
+  dynamic(const std::string& filename, bool has_header) : header_(), data_() {
+    std::ifstream in(filename, std::ios::in | std::ios::binary);
+    readStream(in, has_header);
+  }
+
+  void write(std::ostream& out, bool with_header) {
+    writeStream(out, with_header);
+  }
+
+  void write(const std::string& filename, bool with_header) {
+    std::ofstream out(filename, std::ios::out | std::ios::binary);
+    writeStream(out, with_header);
+  }
+
+  inline size_t header_index(const std::string& column) const {
+    for (size_t i = 0; i < header_.size(); i++) {
+      if (header_.at(i) == column) {
+        return i;
+      }
+    }
+    throw std::runtime_error("Unrecognized column " + column);
+  }
+
+  inline size_t size() const {
+    return data_.size();
+  }
+
+  inline std::string& at(size_t row, const std::string& column) {
+    rangeCheck(row);
+    return data_[row][header_index(column)];
+  }
+
+  inline const std::string& at(size_t row, const std::string& column) const {
+    rangeCheck(row);
+    return data_[row][header_index(column)];
+  }
+
+  inline std::string& at(size_t row, size_t column) {
+    rangeCheck(row, column);
+    return data_[row][column];
+  }
+
+  inline const std::string& at(size_t row, size_t column) const {
+    rangeCheck(row, column);
+    return data_[row][column];
+  }
+
+  template <typename T = std::string>
+  inline T get(size_t row, const std::string& column) const {
+    return detail::convert<T>(at(row, column));
+  }
+
+  template <typename T = std::string>
+  inline T get(size_t row, size_t column) const {
+    return detail::convert<T>(at(row, column));
+  }
+
+  bool operator==(const dynamic& other) const {
+    return header_ == other.header_ && data_ == other.data_;
+  }
+
+  bool operator!=(const dynamic& other) const {
+    return !(*this == other);
+  }
+
+ private:
+  void readStream(std::istream& in, bool has_header) {
+    if (in.bad() || in.fail()) {
+      throw std::runtime_error("Bad input");
+    }
     std::istreambuf_iterator<char> it(in);
     if (has_header && it != detail::EOCSVF) {
-      std::vector<std::string> header = detail::readCsvRow(it);
-      for (const auto& h : header) {
-        if (indices_.count(h) == 0) {
-          size_t i = indices_.size();
-          indices_[h] = i;
-        }
-      }
+      header_ = detail::readCsvRow(it);
     }
     while (it != detail::EOCSVF) {
       std::vector<std::string> row = detail::readCsvRow(it);
-      while (row.size() < indices_.size()) {
+      while (row.size() < header_.size()) {
         row.emplace_back();
       }
       data_.push_back(std::move(row));
     }
   }
 
-  inline std::string& at(size_t row, const std::string& column) {
-    if (row >= data_.size()) {
+  void writeStream(std::ostream& out, bool with_header) {
+    if (out.bad() || out.fail()) {
+      throw std::runtime_error("Bad output file");
+    }
+    std::ostreambuf_iterator<char> it(out);
+    if (with_header) {
+      detail::writeCsvRow(it, header_);
+      it = '\n';
+    }
+    for (size_t i = 0; i < data_.size(); i++) {
+      detail::writeCsvRow(it, data_.at(i));
+      if (i != data_.size() - 1) {
+        it = '\n';
+      }
+    }
+  }
+
+  inline void rangeCheck(size_t row) const {
+    if (row >= size()) {
       throw std::runtime_error("Row " + std::to_string(row) + " out of bounds");
     }
-    auto index_it = indices_.find(column);
-    if (index_it == indices_.end()) {
-      throw std::runtime_error("Unrecognized column " + column);
+  }
+
+  inline void rangeCheck(size_t row, size_t col) const {
+    rangeCheck(row);
+    if (col >= data_[row].size()) {
+      throw std::runtime_error("Column " + std::to_string(col) + " out of bounds");
     }
-    return data_[row][index_it->second];
   }
 
-  inline const std::string& at(size_t row, const std::string& column) const {
-    return at(row, column);
-  }
-
-  template <typename T>
-  inline T get(size_t row, std::string column) const {
-    return T(at(row, column));
-  }
-
- private:
-  std::map<std::string, size_t> indices_;
+  std::vector<std::string> header_;
   std::vector<std::vector<std::string>> data_;
 };
 
-
-
-}  // namespace dcsv
+}  // namespace pHcsv
--- test_pHcsv.cpp
+++ test_pHcsv.cpp
@@ -1,19 +1,53 @@
 #include "pHcsv.h"
 
-#include <fstream>
+#include <assert.h>
+
+template<typename T>
+inline std::string toString(const T& val) {
+  return std::to_string(val);
+}
+
+template<>
+inline std::string toString(const std::string& val) {
+  return val;
+}
+
+#define ASSERT_EQ(expr, expected) if ((expr) != (expected)) { printf("Assert failed at line %d:\n  %s != %s\n", __LINE__, toString(expr).c_str(), #expected); return 1; }
 
 int test_dynamic_wiki() {
-  std::ifstream wiki("../dcsv/test_data/wiki.csv");
-  pHcsv::Dynamic csv_data(wiki, true);
-  std::cout << csv_data.at(1, "Extras") << std::endl;
-  std::cout << csv_data.at(2, "Extras") << std::endl;
-  std::cout << csv_data.at(3, "Extras") << std::endl;
-  std::cout << csv_data.at(2, "Model") << std::endl;
-  std::cout << csv_data.at(3, "Description") << std::endl;
+  pHcsv::dynamic data("../dcsv/test_data/wiki.csv", true);
+
+  // Test get() and at()
+  ASSERT_EQ(data.at(0, "Extras"), "steering \"wheel\"");
+  ASSERT_EQ(data.at(0, "Extras"), data.at(0, 5));
+  ASSERT_EQ(data.at(0, "Extras"), data.get(0, "Extras"));
+  ASSERT_EQ(data.at(0, "Extras"), data.get(0, 5));
+  ASSERT_EQ(data.at(1, "Extras"), "wheels and \"frame\"");
+  ASSERT_EQ(data.at(2, "Extras"), "LED-\"lights\"");
+  ASSERT_EQ(data.at(2, "Model"), "Venture \"Extended Edition, Very Large\"");
+  ASSERT_EQ(data.at(3, "Description"), "MUST SELL!\nair, moon \"\"roof\"\", loaded");
+
+  ASSERT_EQ(data.at(3, "Extras"), "");
+  data.at(3, "Extras") = "new data";
+  ASSERT_EQ(data.at(3, "Extras"), "new data");
+
+  ASSERT_EQ(data.get<int>(0, "Year"), 1997);
+  ASSERT_EQ(data.get<size_t>(3, "Year"), 1996);
+  ASSERT_EQ(data.get<float>(3, "Price"), 4799.0f);
+  ASSERT_EQ(data.get<double>(1, "Price"), 4900.0);
+
+  // Test write()
+  data.write("../dcsv/test_data/wiki_written.csv", true);
+  pHcsv::dynamic written_data("../dcsv/test_data/wiki_written.csv", true);
+  if (data != written_data) {
+    printf("Written data does not match read data\n");
+    return 1;
+  }
+  std::remove("../dcsv/test_data/wiki_written.csv");
+
   return 0;
 }
 
 int main() {
-    test_dynamic_wiki();
-    return 0;
+  return test_dynamic_wiki();
 }
