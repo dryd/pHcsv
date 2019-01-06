0d6c31b0b946b1258d38835af3298f602d3056be
dryd <rydan4@gmail.com>
2019-01-06 15:39:15 +0100

    Slack variables

--- src/pHlp.h
+++ src/pHlp.h
@@ -9,50 +9,65 @@
 
 namespace pH {
 
+enum class constraint_type {
+  LEQ,
+  EQ,
+  GEQ
+};
+
 namespace details {
 
 struct constraint {
-  constraint(std::map<size_t, double> a, double rhs) : a(std::move(a)), rhs(rhs) {}
+  constraint(std::map<size_t, double> a, constraint_type t, double rhs) : a(std::move(a)), t(t), rhs(rhs) {
+    if (t == constraint_type::GEQ || t == constraint_type::EQ) {
+      throw std::runtime_error("Unsupported constraint type");
+    }
+  }
 
   std::map<size_t, double> a;
+  constraint_type t;
   double rhs;
 };
 
 class tableau {
  public:
   tableau(const std::vector<double>& c, const std::vector<constraint>& constraints)
-      : num_rows_(constraints.size() + 1), num_cols_(c.size() + 1),
+      : num_rows_(constraints.size() + 1), num_slacks_(constraints.size()), num_cols_(c.size() + num_slacks_ + 1),
         tableau_(num_rows_ * num_cols_, 0.0), basic_variables_(constraints.size()) {
     for (size_t i = 0; i < c.size(); i++) {
       obj(i) = -c.at(i);
     }
     for (size_t i = 0; i < constraints.size(); i++) {
       for (const auto& el : constraints.at(i).a) {
-        at(i + 1, el.first) = el.second;
+        at(i, el.first) = el.second;
       }
-      rhs(i + 1) = constraints.at(i).rhs;
+      at(i, i + (numVars() - num_slacks_)) = 1.0;
+      rhs(i) = constraints.at(i).rhs;
       basic_variables_.at(i) = i + (c.size() - constraints.size());
     }
   }
 
-  std::pair<size_t, double> pivotColumn() const {
+  size_t pivotColumn() const {
     std::pair<size_t, double> pivot(0, obj(0));
-    for (size_t i = 1; i < numVars(); i++) {
-      if (obj(i) < pivot.second) {
-        pivot.first = i;
-        pivot.second = obj(i);
+    for (size_t col = 1; col < numVars(); col++) {
+      if (obj(col) < pivot.second) {
+        pivot.first = col;
+        pivot.second = obj(col);
       }
     }
-    return pivot;
+    if (pivot.second >= 0.0) {
+      return std::numeric_limits<size_t>::max();
+    }
+    return pivot.first;
   }
 
   size_t pivotRow(size_t pivot_column) const {
     std::pair<size_t, double> pivot_row(std::numeric_limits<size_t>::max(), std::numeric_limits<double>::max());
-    for (size_t i = 1; i < num_rows_; i++) {
-      if (at(i, pivot_column) > 0.0 && rhs(i) > 0.0) {
-        double pivot = rhs(i) / at(i, pivot_column);
+    for (size_t row = 0; row < num_rows_ - 1; row++) {
+      if (at(row, pivot_column) > 0.0 && rhs(row) > 0.0) {
+        double pivot = rhs(row) / at(row, pivot_column);
         if (pivot < pivot_row.second) {
-          pivot_row.first = i;
+          pivot_row.first = row;
           pivot_row.second = pivot;
         }
       }
@@ -78,13 +93,16 @@ class tableau {
     for (size_t i = 0; i < num_cols_; i++) {
       at(pivot_row, i) /= pivot;
     }
-    basic_variables_.at(pivot_row - 1) = pivot_col;
+    basic_variables_.at(pivot_row) = pivot_col;
   }
 
   std::pair<std::vector<double>, double> solution() const {
-    std::pair<std::vector<double>, double> result(std::vector<double>(numVars(), 0.0), rhs(0));
+    std::pair<std::vector<double>, double> result(std::vector<double>(numVars() - num_slacks_, 0.0), rhs(num_rows_ - 1));
     for (size_t i = 0; i < basic_variables_.size(); i++) {
-      result.first.at(basic_variables_.at(i)) = rhs(i + 1);
+      if (basic_variables_.at(i) >= result.first.size()) {
+        continue;
+      }
+      result.first.at(basic_variables_.at(i)) = rhs(i);
     }
     return result;
   }
@@ -104,8 +122,8 @@ class tableau {
   double& at(size_t row, size_t col) { return tableau_.at(col * num_rows_ + row); }
   double at(size_t row, size_t col) const { return tableau_.at(col * num_rows_ + row); }
 
-  double& obj(size_t var) { return at(0, var); }
-  double obj(size_t var) const { return at(0, var); }
+  double& obj(size_t var) { return at(num_rows_ - 1, var); }
+  double obj(size_t var) const { return at(num_rows_ - 1, var); }
 
   double& rhs(size_t row) { return at(row, num_cols_ - 1); }
   double rhs(size_t row) const { return at(row, num_cols_ - 1); }
@@ -113,6 +131,7 @@ class tableau {
   size_t numVars() const { return num_cols_ - 1; }
 
   size_t num_rows_;
+  size_t num_slacks_;
   size_t num_cols_;
   std::vector<double> tableau_;
   std::vector<size_t> basic_variables_;
@@ -123,14 +142,13 @@ class tableau {
 class lp {
  public:
   lp(std::vector<double> objective_coefficients)
-    : //num_slacks_(0),
-      c_(std::move(objective_coefficients)),
+    : c_(std::move(objective_coefficients)),
       constraints_() {}
 
   size_t numVariables() const { return c_.size(); }
 
-  size_t addConstraint(std::map<size_t, double> constraint, double rhs) {
-    constraints_.emplace_back(std::move(constraint), rhs);
+  size_t addConstraint(std::map<size_t, double> constraint, constraint_type type, double rhs) {
+    constraints_.emplace_back(std::move(constraint), type, rhs);
     return constraints_.size() - 1;
   }
 
@@ -138,26 +156,23 @@ class lp {
     details::tableau t(c_, constraints_);
     std::cout << t.toString() << std::endl;
 
-    while (true) {
-      auto entering_pivot = t.pivotColumn();
-      if (entering_pivot.second >= 0.0) {
-        break;
-      }
-      std::cout << entering_pivot.first << ", " << entering_pivot.second << std::endl;
-      size_t leaving_pivot = t.pivotRow(entering_pivot.first);
+    size_t entering_pivot = t.pivotColumn();
+    while (entering_pivot != std::numeric_limits<size_t>::max()) {
+      std::cout << entering_pivot << std::endl;
+      size_t leaving_pivot = t.pivotRow(entering_pivot);
       std::cout << leaving_pivot << std::endl;
       if (leaving_pivot == std::numeric_limits<size_t>::max()) {
-        break;
+        throw std::runtime_error("Unbounded model");
       }
-      t.pivot(entering_pivot.first, leaving_pivot);
+      t.pivot(entering_pivot, leaving_pivot);
       std::cout << t.toString() << std::endl;
+      entering_pivot = t.pivotColumn();
     }
 
     return t.solution();
   }
 
  private:
-  //size_t num_slacks_;
   std::vector<double> c_;
   std::vector<details::constraint> constraints_;
 };
--- test_pHlp/test_pHlp.cpp
+++ test_pHlp/test_pHlp.cpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <chrono>
+#include <cmath>
 
 template<typename T>
 std::string toString(const T& val) {
@@ -28,6 +29,22 @@ std::string toString(const std::string& val) {
   return val;
 }
 
+bool relEq(double value, double expected, double tol) {
+  if (expected == 0.0) return std::abs(value) < tol;
+  return std::abs((value - expected) / expected) < tol;
+}
+
+bool relEq(std::vector<double> values, std::vector<double> expecteds, double tol) {
+  if (values.size() != expecteds.size()) return false;
+  for (size_t i = 0; i < values.size(); i++) {
+    if (expecteds.at(i) == 0.0) return std::abs(values.at(i)) < tol;
+    if (std::abs((values.at(i) - expecteds.at(i)) / expecteds.at(i)) > tol) return false;
+  }
+  return true;
+}
+
+#define ASSERT_REL_EQ(expr, expected, tol) if (!relEq((expr), (expected), (tol))) { printf("Assert failed at line %d:\n  %s !~= %s\n", __LINE__, toString(expr).c_str(), toString(expected).c_str()); return 1; }
+
 #define ASSERT_EQ(expr, expected) if ((expr) != (expected)) { printf("Assert failed at line %d:\n  %s != %s\n", __LINE__, toString(expr).c_str(), #expected); return 1; }
 
 double getDuration(std::chrono::time_point<std::chrono::high_resolution_clock> start) {
@@ -35,28 +52,31 @@ double getDuration(std::chrono::time_point<std::chrono::high_resolution_clock> s
 }
 
 int test1() {
-  pH::lp lp({3, 2, 0, 0, 0});
-  lp.addConstraint({{0, 2}, {1, 1}, {2, 1}}, 18);
-  lp.addConstraint({{0, 2}, {1, 3}, {3, 1}}, 42);
-  lp.addConstraint({{0, 3}, {1, 1}, {4, 1}}, 24);
+  pH::lp lp({3, 2});
+  lp.addConstraint({{0, 2}, {1, 1}}, pH::constraint_type::LEQ, 18);
+  lp.addConstraint({{0, 2}, {1, 3}}, pH::constraint_type::LEQ, 42);
+  lp.addConstraint({{0, 3}, {1, 1}}, pH::constraint_type::LEQ, 24);
   auto solution = lp.optimize();
-  std::vector<double> expected{1.5, 1.0, 0.0, 5.5, 3.0, 0.0};
-  ASSERT_EQ(solution.first, expected);
+  std::vector<double> expected{3.0, 12.0};
+  ASSERT_REL_EQ(solution.first, expected, 1e-10);
+  ASSERT_REL_EQ(solution.second, 33.0, 1e-10);
   return 0;
 }
 
 int test2() {
-  pH::lp lp({4, 3, 0, 0, 0, 0});
-  lp.addConstraint({{0, 2}, {1, 3}, {2, 1}}, 6);
-  lp.addConstraint({{0, -3}, {1, 2}, {3, 1}}, 3);
-  lp.addConstraint({{1, 2}, {4, 1}}, 5);
-  lp.addConstraint({{0, 2}, {1, 1}, {5, 1}}, 4);
+  pH::lp lp({4, 3});
+  lp.addConstraint({{0, 2}, {1, 3}}, pH::constraint_type::LEQ, 6);
+  lp.addConstraint({{0, -3}, {1, 2}}, pH::constraint_type::LEQ, 3);
+  lp.addConstraint({{1, 2}}, pH::constraint_type::LEQ, 5);
+  lp.addConstraint({{0, 2}, {1, 1}}, pH::constraint_type::LEQ, 4);
   auto solution = lp.optimize();
-  std::vector<double> expected{1.5, 1.0, 0.0, 5.5, 3.0, 0.0};
-  ASSERT_EQ(solution.first, expected);
+  std::vector<double> expected{1.5, 1.0};
+  ASSERT_REL_EQ(solution.first, expected, 1e-10);
+  ASSERT_REL_EQ(solution.second, 9.0, 1e-10);
   return 0;
 }
 
 int main() {
-  return test1() + test2();
+  return test1() +
+         test2();
 }
